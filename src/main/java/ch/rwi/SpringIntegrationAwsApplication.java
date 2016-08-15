a7fc8b0524dbeae374e30220f7381998fdda73dd
René Winkler <renewinkler@gmx.ch>
2016-08-15 15:06:39 +0200

    initial setup

---
+++ src/main/java/ch/rwi/SpringIntegrationAwsApplication.java
@@ -0,0 +1,12 @@
+package ch.rwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationAwsApplication.class, args);
+	}
+}
